318261bd0c1628e0818f28b08404c4cd5d1ab58c
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2024-04-02 18:49:10 +0300

    updated no of viewable components

--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -11,7 +11,7 @@ const Testimonials = () => {
     dots: true,
     infinite: true,
     speed: 200,
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
