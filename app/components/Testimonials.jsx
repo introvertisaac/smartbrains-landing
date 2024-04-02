c2886cd827695f894cdca336712e54a686396cd4
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2024-04-02 19:10:16 +0300

    updated carousel

--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -10,39 +10,36 @@ const Testimonials = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 200,
+    speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    arrows: true, // Add navigation buttons
     responsive: [
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
         },
       },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
         },
       },
     ],
   };
 
   return (
-    <div className="mt-20 mx-auto max-w-7xl px-4 overflow-x-hidden" style={{ height: "600px" }}>
-      <div className="flex justify-center text-center mb-8">
-        <h2 className="text-3xl font-bold">What Our Parents and Partners Say</h2>
+    <div className="mt-20 mx-auto max-w-7xl px-4 overflow-x-hidden" style={{ height: "480px" }}>
+      <div className="flex justify-center mb-8">
+        <h2 className="text-3xl font-bold">What Our Customers Say</h2>
       </div>
-      <Slider {...settings} className="slick-list">
+      <Slider {...settings} className="mb-8">
         {testimonialsData.map((testimonial) => (
-          <div key={testimonial.id} className="px-2 md:px-4">
-            <div className="bg-white rounded-lg shadow-md p-6 h-72 w-64 md:w-72 flex flex-col">
+          <div key={testimonial.id} className="px-4">
+            <div className="bg-white rounded-lg shadow-md p-6 h-80 w-70 md:w-70 flex flex-col">
               <div className="flex items-center mb-4">
                 <img
                   className="w-16 h-16 rounded-full mr-4"
@@ -66,11 +63,18 @@ const Testimonials = () => {
           </div>
         ))}
       </Slider>
-      <style jsx>{`
-        .slick-list {
-          overflow: hidden;
+      <div className="pt-5">
+      <style jsx global>{`
+        .slick-dots li.slick-active button:before {
+          color: blue;
+          font-size: 15px;
+        }
+        .slick-dots li button:before {
+          color: #F1948A;
+          font-size: 12px;
         }
       `}</style>
+      </div>
     </div>
   );
 };
